6601eb9b519ccfa3829412cf6246b6d4dd921d61
MoneroOcean <support@moneroocean.stream>
2018-08-26 19:02:49 +0200

    First implementation draft

--- sickle-core.cpp
+++ sickle-core.cpp
@@ -196,6 +196,7 @@ class Simple: public AsyncWorker {
                         puts("XXXX5");
                         if (ways != new_ways || mem != new_mem) {
                             for (unsigned i = 0; i != ways; ++i) if (ctx[i]->memory) _mm_free(ctx[i]->memory); // free previous ways
+                        puts("XXXX7");
                             ways = new_ways;
                             mem  = new_mem;
                             for (unsigned i = 0; i != ways; ++i) ctx[i]->memory = static_cast<uint8_t *>(_mm_malloc(mem, 4096));
