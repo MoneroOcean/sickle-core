274f350e22cdfb3de1c53cceb52e1f1e0902b673
MoneroOcean <support@moneroocean.stream>
2018-08-26 19:28:57 +0200

    First implementation draft

--- sickle-core.cpp
+++ sickle-core.cpp
@@ -214,6 +214,7 @@ class Simple: public AsyncWorker {
                             *p_nonce(blob, blob_len, i) = nonce++;
                         }
                         fn = pi_fn->second;
+                        printf("!!! "PRIu64"\n", target);
                  
                     } else if (pi->name == "pause") {
                         fn = nullptr;
